fc0a8062a7d5a5ddce9b66721df7f26c65ccc97c
Nadfe <62165074+Nadfe@users.noreply.github.com>
2021-05-22 16:52:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const Bot = require("./src/index"),
 
 
 new Bot({
-    owners: [], // YOUR USER ID IN HERE
+    owners: [813469762130280470], // YOUR USER ID IN HERE
     prefix: "?", // DEFAULT IS ?
-    token: "", // YOUR BOT TOKEN  (DO NOT SHARE WITH ANYONE)
+    token: "ODI0Njk4MjY0Mjk2ODgyMjI2.YFzKHA.pLtqNZMwkxk9dEu-6BOmYT1JG3c", // YOUR BOT TOKEN  (DO NOT SHARE WITH ANYONE)
     // embed: {} // on: {}, off: {} // refer to the readme
     watch: [
         // The user/bot's ID, The full webhook URL, the guildID that you want the bot to watch for, the roleID and if you want it enabled.
